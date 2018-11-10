f8f2b1fa5ed1a9fe555b0e8b6ed604a735132602
ValtersJZ <valters.zakrevskis@gmail.com>
2018-11-10 20:10:37 +0000

    yup

--- flower/flower.ino
+++ flower/flower.ino
@@ -1,10 +1,12 @@
-
 #include <Arduino.h>
 #include <Hash.h>
 #include <ESP8266WiFi.h>
 
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
+
+#define LED     2
+
 
 /* Set these to your desired credentials. */
 String flowerID = "1";
@@ -13,7 +15,9 @@ unsigned int count = 0;
 
 String key = "69696969";
 String IP = "192.168.4.1";
+
 unsigned int pollen = 100;
+bool pollen_updated = false;
 
 ESP8266WebServer server(80);
 
@@ -24,13 +28,16 @@ void handleRoot() {
   count++;
   String values = flowerID + ',' + String(count) + ',' + String(pollen) + ',';
   
-  String msg = "http://" + IP + "/" + values + ',' + sha1(values + key);
+  String msg = "http://" + IP + "/" + values + sha1(values + key);
   server.send(200, "text/html", "<h1>" + msg + "</h1>");
-  
+
+  pollen = 0;  
   delay(250);
 }
 
 void setup() {
+  pinMode(LED, OUTPUT);   // LED pin as output. 
+  
   delay(1000);
   Serial.begin(115200);
   Serial.println();
@@ -49,5 +56,19 @@ void setup() {
 }
 
 void loop() {
+  
   server.handleClient();
+  
+  if( millis() % 1000 == 0 && pollen < 100){
+    if(!pollen_updated){
+      pollen_updated = true;
+      pollen++;
+
+      int val = map(pollen, 0, 100, 1023, 0);
+      analogWrite(LED, val);
+    }
+    
+  }else{
+    pollen_updated = false;
+  }
 }
